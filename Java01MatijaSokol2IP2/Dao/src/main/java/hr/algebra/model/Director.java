9a6c47cc51e37ad56ffb630b74956f3c28af2756
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-05-14 09:26:30 +0200

    Director get and set

--- Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/model/Director.java
+++ Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/model/Director.java
@@ -10,4 +10,20 @@ class Director {
         this.LastName = LastName;
     }
 
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
 }
