c70b9c02e6fe424627defd3690fae4e85f0551e1
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-05-14 09:22:47 +0200

    Actor getters and setters

--- Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/model/Actor.java
+++ Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/model/Actor.java
@@ -10,4 +10,20 @@ public class Actor {
         this.LastName = LastName;
     }
 
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
 }
