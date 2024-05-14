f60f7c28fea279005ca7e9bc2368d316919381f0
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-05-14 08:49:31 +0200

    Movie fields add

--- Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/model/Movie.java
+++ Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/model/Movie.java
@@ -1,6 +1,25 @@
 package hr.algebra.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 public class Movie {
-    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+    //Mon, 13 May 2024 22:00:00 GMT
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.RFC_1123_DATE_TIME;
+
+    private int id;
+    private String title;
+    private LocalDateTime publishedDate;
+    private String description;
+    private String originalTitle;
+    private Director director;
+    private Actor actor;
+    private int duration;
+    private String link;
+    private int year;
+    private List<Genre> genres
+    private String picturePath;
 
 }
