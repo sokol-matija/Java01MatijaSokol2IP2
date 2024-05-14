5b2c1063970aabc99dd799ea741ed68adb16827b
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-05-14 14:27:41 +0200

    Genre class refactor

--- Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/model/Genre.java
+++ Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/model/Genre.java
@@ -1,6 +1,34 @@
 package hr.algebra.model;
 
-
 public class Genre {
+
+    public int id;
+    public String type;
+
+    public Genre(int id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" + "id=" + id + ", type=" + type + '}';
+    }
 
 }
