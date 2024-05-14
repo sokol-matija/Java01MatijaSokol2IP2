85948c9f448ed97cdfe9f8a10bea619755cede17
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-05-14 12:56:11 +0200

    Actor add id prop, get and set id,

--- Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/model/Actor.java
+++ Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/model/Actor.java
@@ -2,12 +2,22 @@ package hr.algebra.model;
 
 public class Actor {
 
+    private int id;
     private String FirstName;
     private String LastName;
 
-    public Actor(String FirstName, String LastName) {
+    public Actor(int id, String FirstName, String LastName) {
+        setId(id);
         setFirstName(FirstName);
         setLastName(LastName);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getFirstName() {
