88df1689fd1b6ac494fbf937af43b89f00ec38ed
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-05-14 09:36:15 +0200

    DataSourceSingleton add constants for SQL connection

--- Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/dal/sql/DataSourceSingleton.java
+++ Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/dal/sql/DataSourceSingleton.java
@@ -1,6 +1,15 @@
 package hr.algebra.dal.sql;
 
+import java.util.Properties;
 
 public class DataSourceSingleton {
 
+    private static final String PATH = "/config/db.properties";
+
+    private static final String SERVER_NAME = "SERVER_NAME";
+    private static final String DATABASE_NAME = "DATABASE_NAME";
+    private static final String USER = "USER";
+    private static final String PASSWORD = "PASSWORD";
+
+    private static final Properties PROPERTIES = new Properties();
 }
