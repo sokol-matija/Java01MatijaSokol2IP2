0d02f5e3981d435b9ed762d2e2a5ad1bad7285a6
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-05-14 09:22:07 +0200

    Actor ctor

--- Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/model/Actor.java
+++ Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/model/Actor.java
@@ -4,4 +4,10 @@ public class Actor {
 
     private String FirstName;
     private String LastName;
+
+    public Actor(String FirstName, String LastName) {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+    }
+
 }
