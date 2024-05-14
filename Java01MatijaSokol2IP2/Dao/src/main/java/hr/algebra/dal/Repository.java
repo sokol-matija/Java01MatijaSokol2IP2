b1ac0008534af2f8a5ece9e1eb88bd84399a787e
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-05-14 14:15:07 +0200

    Repository CRUD interface for all tables

--- Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/dal/Repository.java
+++ Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/dal/Repository.java
@@ -1,11 +1,16 @@
 package hr.algebra.dal;
 
+import hr.algebra.model.Actor;
+import hr.algebra.model.Director;
+import hr.algebra.model.Genre;
 import hr.algebra.model.Movie;
 import java.util.List;
 import java.util.Optional;
 
 public interface Repository {
+    //TODO: Separate in dif inferface for each table:
 
+    //Movies
     int createMovie(Movie movie) throws Exception;
 
     void createMovies(List<Movie> movies) throws Exception;
@@ -17,4 +22,44 @@ public interface Repository {
     Optional<Movie> selectMovie(int id) throws Exception;
 
     List<Movie> selectMovies() throws Exception;
+
+    //Actors
+    int createActor(Actor actor) throws Exception;
+
+    void createActors(List<Actor> actors) throws Exception;
+
+    void updateActor(int id, Actor actor) throws Exception;
+
+    void deleteActor(int id) throws Exception;
+
+    Optional<Actor> selectActor(int id) throws Exception;
+
+    List<Actor> selectActors() throws Exception;
+
+    //Genre
+    int createGenre(Genre genre) throws Exception;
+
+    void createGenres(List<Genre> genres) throws Exception;
+
+    void updateGenre(int id, Genre genre) throws Exception;
+
+    void deleteGenre(int id) throws Exception;
+
+    Optional<Genre> selectGenre(int id) throws Exception;
+
+    List<Genre> selectGenres() throws Exception;
+
+    //Director
+    int createDirector(Director director) throws Exception;
+
+    void createDirectors(List<Director> directors) throws Exception;
+
+    void updateDirector(int id, Director director) throws Exception;
+
+    void deleteDirector(int id) throws Exception;
+
+    Optional<Director> selectDirector(int id) throws Exception;
+
+    List<Director> selectDirectors() throws Exception;
+
 }
