85c1edc1e7e9d268aa02622825bae2d8def75b24
sokol.matija <sokol.matija@gmail.com>
2024-05-28 15:02:10 +0200

    JAXBUtils create
    serialize and deserialize Java objects to and from XML files using JAXB (Java Architecture for XML Binding)

---
+++ Java01MatijaSokol2IP2/Utilities/src/main/java/hr/algebra/utilities/JAXBUtlils.java
@@ -0,0 +1,20 @@
+package hr.algebra.utilities;
+
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+public class JAXBUtlils {
+
+    private JAXBUtlils() {
+    }
+
+    public static void save(Object object, String filename) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(object.getClass());
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(object, new File(filename));
+    }
+
+}
