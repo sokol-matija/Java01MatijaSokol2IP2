c5b949cc923ae7ec56e82ee2c82ceec79173206f
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-06-21 09:34:15 +0200

    refactor DAO Director and Actor to Person class

---
+++ Java01MatijaSokol2IP2/Dao/src/main/java/hr/algebra/model/Person.java
@@ -0,0 +1,57 @@
+package hr.algebra.model;
+
+public class Person {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Person(int id, String FirstName, String LastName) {
+        this.id = id;
+        this.firstName = FirstName;
+        this.lastName = LastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.firstName = FirstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.lastName = LastName;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Person other = (Person) obj;
+        return this.id == other.id;
+    }
+
+}
